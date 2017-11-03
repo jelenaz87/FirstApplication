4351c799f64e9c395943567a525d3ee2dfe8ee1f
ENDAVA\jelena.zivanovic <jelena.zivanovic@endava.com>
2017-11-03 13:26:22 +0100

    Initial commit

---
+++ app/src/main/java/com/example/jelenazivanovic/myapplication/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.jelenazivanovic.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mButton = findViewById(R.id.button);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mButton.setText("kliknuto");
+            }
+        });
+    }
+}
